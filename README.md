e7f54ca09712ba0153d13b871018e35f79ea93af
Pablo  García <pablogarcia@MacBook-Pro-de-Pablo.local>
2024-10-31 15:53:29 -0500

    Ultimos cambios

--- README.md
+++ README.md
@@ -1,14 +1,15 @@
 # Proyecto ETL con Docker Compose
 
 ## Descripción
-Este proyecto carga datos desde un archivo CSV a una base de datos PostgreSQL usando un contenedor Docker. El sistema está orquestado con Docker Compose para incluir tanto la base de datos como la aplicación ETL.
+Este proyecto carga datos desde un archivo CSV a una base de datos PostgreSQL usando un contenedor de Docker. El sistema está orquestado con Docker Compose para incluir tanto la base de datos como la aplicación ETL.
 
 ## Requisitos
 - Docker
 - Docker Compose
+- Tener un entorno donde se peuda ejecutar python y descargar sus librerías
 
 ## Estructura de Archivos
-- `docker-compose.yml`: Configura los servicios de la base de datos y la aplicación Python.
+- `docker-compose.yml`: Configuració de los servicios de la base de datos y la aplicación Python.
 - `Dockerfile`: Construye el contenedor de la aplicación ETL.
 - `script.py`: Script Python que realiza la carga de datos.
 - `requirements.txt`: Lista de dependencias para el script Python.
@@ -17,7 +18,7 @@ Este proyecto carga datos desde un archivo CSV a una base de datos PostgreSQL us
 ## Instrucciones
 
 1. Clona el repositorio o descarga el proyecto en tu máquina.
-2. Asegúrate de que el archivo `fact_table.csv` esté en la raíz del proyecto.
+2. Asegúrate de que el archivo `facts_table.csv` esté en la raíz del proyecto.
 3. Construye y ejecuta los contenedores con Docker Compose:
    ```bash
    docker compose up
--- script.py
+++ script.py
@@ -1,3 +1,4 @@
+#Se hacen las importaciones necesarias para el desarrollo del script
 import pandas as pd
 import psycopg2
 import warnings
@@ -5,16 +6,22 @@ from dotenv import load_dotenv
 import os
 warnings.filterwarnings('ignore')
 
+#Se sube el archivo facts_table.csv para leerlo y extraer los datos desde ahí
 df = pd.read_csv('./facts_table.csv')
 
+#Se crean las columnas type_sales, discount, discount_percentage, profit y profit_margin, 
+#para luego ser agregadas al archivo Finaldf.csv. Estas columnas se crean a partir de las columnas 
+# ya existentes con el fin de que sean de utilidad dentro de análisis de los datos.
 df['type_sales'] = df['amount'].apply(lambda x: 'high sales' if x > 100.000 else 'low sales')
 df['discount'] = df['amount'] * df['discount']
 df['discount_percentage'] = (df['discount'] / df['amount']) * 100
 df['profit'] = df['amount'] - df['freight']
 df['profit_margin'] = (df['profit'] / df['amount']) * 100
 
+#Se guarda el archivo Finaldf.csv con las columnas creadas
 df.to_csv('Finaldf.csv', sep = '|', index = False)
 
+#Se establece la conexión con la base de datos
 load_dotenv()
 
 host = os.getenv('DB_HOST')
@@ -27,9 +34,13 @@ connection = psycopg2.connect(host=host, database=database,
                               user=user, password=password, port=port)
 cursor = connection.cursor()
 
+#Se crea la tabla facts_table en la base de datos
 cursor.execute('CREATE TABLE IF NOT EXISTS facts_table (facts_id SMALLINT, order_id SMALLINT, freight REAL, order_date DATE, unit_price REAL, quantity SMALLINT, discount REAL, amount REAL, product_id SMALLINT, supplier_id SMALLINT, category_id SMALLINT, employee_id SMALLINT, customer_id CHARACTER VARYING(5), type_sales CHARACTER VARYING(20), discount_percentage REAL, profit REAL, profit_margin REAL);')
 connection.commit()
+#Se establece la columna facts_id como llave primaria
 cursor.execute("""ALTER TABLE ONLY facts_table ADD CONSTRAINT pk_facts_id PRIMARY KEY (facts_id)""")
+
+#Se copian los datos del archivo Finaldf.csv a la tabla facts_table para poblarla. 
 
 with open('Finaldf.csv', 'r') as f:
     next(f)
