206fad93059866623d4db33f0421ba6b90cac804
Pablo  García <pablogarcia@MacBook-Pro-de-Pablo.local>
2024-10-31 14:13:09 -0500

    Subiendo todo a un repositorio

--- script.py
+++ script.py
@@ -1,6 +1,8 @@
 import pandas as pd
 import psycopg2
 import warnings
+from dotenv import load_dotenv
+import os
 warnings.filterwarnings('ignore')
 
 df = pd.read_csv('./facts_table.csv')
@@ -9,12 +11,20 @@ df['type_sales'] = df['amount'].apply(lambda x: 'high sales' if x > 100.000 else
 df['discount'] = df['amount'] * df['discount']
 df['discount_percentage'] = (df['discount'] / df['amount']) * 100
 df['profit'] = df['amount'] - df['freight']
-df['profit_margin'] = (df['revenue'] / df['amount']) * 100
+df['profit_margin'] = (df['profit'] / df['amount']) * 100
 
 df.to_csv('Finaldf.csv', sep = '|', index = False)
 
-connection = psycopg2.connect(host='localhost', database='ETL_Celerix', 
-                              user='postgres', password='FortyfivePaca4577', port='5432')
+load_dotenv()
+
+host = os.getenv('DB_HOST')
+database = os.getenv('DB_DATABASE')
+user = os.getenv('DB_USER')
+password = os.getenv('DB_PASSWORD')
+port = os.getenv('DB_PORT')
+
+connection = psycopg2.connect(host=host, database=database, 
+                              user=user, password=password, port=port)
 cursor = connection.cursor()
 
 cursor.execute('CREATE TABLE IF NOT EXISTS facts_table (facts_id SMALLINT, order_id SMALLINT, freight REAL, order_date DATE, unit_price REAL, quantity SMALLINT, discount REAL, amount REAL, product_id SMALLINT, supplier_id SMALLINT, category_id SMALLINT, employee_id SMALLINT, customer_id CHARACTER VARYING(5), type_sales CHARACTER VARYING(20), discount_percentage REAL, profit REAL, profit_margin REAL);')
